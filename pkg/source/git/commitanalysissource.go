9761d1b65e70ced0ef2b28ac89e7c565d47aa21d
zdrgeo <zdravko.georgiev@gmail.com>
2024-08-17 16:48:36 +0300

    Start implementing Git commit analysis source

--- pkg/source/git/commitanalysissource.go
+++ pkg/source/git/commitanalysissource.go
@@ -2,8 +2,13 @@ package git
 
 import (
 	"errors"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/zdrgeo/osmium/pkg/analysis"
 )
 
@@ -14,8 +19,123 @@ func NewCommitAnalysisSource() *CommitAnalysisSource {
 	return &CommitAnalysisSource{}
 }
 
-func (source *CommitAnalysisSource) Query(options map[string]string) *analysis.Analysis {
-	log.Fatal(errors.New("not implemented"))
+func (source *CommitAnalysisSource) Query(options map[string]string) (*analysis.Analysis, error) {
+	repositoryURL, ok := options["repository-url"]
 
-	return nil
+	if !ok {
+		return nil, errors.New("missing required source option repository-url")
+	}
+
+	repositoryPath, ok := options["repository-path"]
+
+	if !ok {
+		return nil, errors.New("missing required source option repository-path")
+	}
+
+	cloneOptions := &git.CloneOptions{
+		URL:      repositoryURL,
+		Progress: os.Stdout,
+	}
+
+	repository, err := git.PlainClone(repositoryPath, false, cloneOptions)
+	// repository, err := git.PlainOpen(repositoryPath)
+	// repository, err := git.Clone(memory.NewStorage(), nil, cloneOptions)
+	// repository, err := git.Open(memory.NewStorage(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reference, err := repository.Head()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Can be used later
+	since := time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)
+	until := time.Date(2200, 1, 1, 0, 0, 0, 0, time.UTC)
+	//
+	logOptions := &git.LogOptions{From: reference.Hash(), Since: &since, Until: &until}
+
+	commitIter, err := repository.Log(logOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	modules := map[string]*analysis.Module{}
+	spans := map[string]*analysis.Span{}
+	changes := map[string]*analysis.Change{}
+	nodes := map[string]*analysis.Node{}
+
+	err = commitIter.ForEach(func(commit *object.Commit) error {
+		changeName := fmt.Sprint(commit.Hash)
+
+		changes[changeName] = &analysis.Change{Name: changeName}
+
+		nodeFileIter, err := commit.Files()
+
+		if err != nil {
+			return err
+		}
+
+		err = nodeFileIter.ForEach(func(nodeFile *object.File) error {
+			if node, ok := nodes[nodeFile.Name]; ok {
+				edgeFileIter, err := commit.Files()
+
+				if err != nil {
+					return err
+				}
+
+				err = edgeFileIter.ForEach(func(edgeFile *object.File) error {
+					if edge, ok := node.Edges[edgeFile.Name]; ok {
+						edge.ChangeNames = append(edge.ChangeNames, changeName)
+					} else {
+						node.Edges[edgeFile.Name] = &analysis.Edge{ChangeNames: []string{changeName}}
+					}
+
+					return nil
+				})
+
+				if err != nil {
+					return err
+				}
+			} else {
+				edges := map[string]*analysis.Edge{}
+
+				edgeFileIter, err := commit.Files()
+
+				if err != nil {
+					return err
+				}
+
+				err = edgeFileIter.ForEach(func(edgeFile *object.File) error {
+					edges[edgeFile.Name] = &analysis.Edge{ChangeNames: []string{changeName}}
+
+					return nil
+				})
+
+				if err != nil {
+					return err
+				}
+
+				nodes[nodeFile.Name] = &analysis.Node{Edges: edges}
+			}
+
+			return nil
+		})
+
+		return nil
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	span := &analysis.Span{Changes: changes, Nodes: nodes}
+
+	spans[span.Name] = span
+
+	return &analysis.Analysis{Modules: modules, Spans: spans}, nil
 }
